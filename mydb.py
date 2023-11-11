2d8f6b419ebb1c1b30e3c11a0484ce99b9e034b3
Michal Surmacz <michal.surmacz@icloud.com>
2023-11-11 17:25:10 +0100

    secure sensitive information

--- mydb.py
+++ mydb.py
@@ -1,13 +1,18 @@
+import os
 import mysql.connector
+from dotenv import load_dotenv
 
+# Load environment variables from .env file
+load_dotenv()
+
+# Get database credentials from environment variables
+host = os.getenv("DB_HOST")
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+
+# Create a database connection
 dataBase = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    passwd='ZCamrus1234',
+    host=host,
+    user=user,
+    passwd=password,
 )
-
-cursorObject = dataBase.cursor()
-
-cursorObject.execute("CREATE DATABASE CRM")
-
-print("All Done!")
